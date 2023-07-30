0fffb1c9950e0f588166a3be28c7425a709d8b24
Costa Joseff <rielisson.joseff.costa@ccc.ufcg.edu.br>
2023-07-30 12:02:16 -0300

    Pagina funcional, barreiras criadas e resultado formatado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -14,7 +15,8 @@ import { AreaDeConversaoComponent } from './components/area-de-conversao/area-de
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/area-de-conversao/area-de-conversao.component.ts
+++ src/app/components/area-de-conversao/area-de-conversao.component.ts
@@ -7,12 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AreaDeConversaoComponent {
   private moedas: any[] = [];
+  private moedasSTR: String[] = [];
+  public moedaSelecionada1: any;
+  public moedaSelecionada2: any;
+  private moeda1FoiEscolhida: boolean;
+  private moeda2FoiEscolhida: boolean;
+  private valorFoiEscolhido: boolean;
+  public inputNumero: number;
+  public resultado: number;
+
+  constructor() {
+    this.moeda1FoiEscolhida = false;
+    this.moeda2FoiEscolhida = false;
+    this.valorFoiEscolhido = false;
+    this.inputNumero = 0;
+    this.resultado = 0;
+  }
 
   ngOnInit() {
     this.solicitarMoedas()
       .then((resposta: any) => {
         this.moedas = Object.values(resposta);
-      })
+      });
+
+      setTimeout(() => {
+        for (let elemento of this.moedas) {
+          this.moedasSTR.push(`${elemento.code} - ${elemento.description}`);
+        }
+      }, 500);
   }
 
   private solicitarMoedas() {
@@ -30,7 +52,60 @@ export class AreaDeConversaoComponent {
     });
   }
 
+  public inputCheck() {
+    this.valorFoiEscolhido = this.inputNumero !== null && this.inputNumero != 0;
+  }
+
+  public botaoDisponivel() {
+    setTimeout(() => {}, 500)
+    return this.valorFoiEscolhido && this.moeda1FoiEscolhida && this.moeda2FoiEscolhida;
+  }
+
+  public codigoMoedaSelecionada1() {
+    if (this.moedaSelecionada1 === undefined) {
+      return "-";
+    }
+
+    this.moeda1FoiEscolhida = true;
+    return this.moedaSelecionada1.split(" ")[0];
+  }
+
+  public codigoMoedaSelecionada2() {
+    if (this.moedaSelecionada2 === undefined) {
+      return "-";
+    }
+
+    this.moeda2FoiEscolhida = true;
+    return this.moedaSelecionada2.split(" ")[0];
+  }
+
+  public debug() {
+    //console.log(this.moedaSelecionada.split());
+  }
+
   public getMoedas() {
-    return this.moedas;
+    return this.moedasSTR;
+  }
+
+  public converter() {
+    this.solicitarConversao()
+      .then((resposta: any) => {
+        this.resultado = resposta.toFixed(2);
+      });
+  }
+
+  private solicitarConversao() {
+    return new Promise((resolve) => {
+      var requestURL = `https://api.exchangerate.host/convert?from=${this.moedaSelecionada1.split(" ")[0]}&to=${this.moedaSelecionada2.split(" ")[0]}&amount=${this.inputNumero}`; 
+      var request = new XMLHttpRequest(); 
+      request.open('GET', requestURL);
+      request.responseType = 'json';
+      request.send();
+
+      request.onload = function() {
+        var response = request.response;
+        resolve(response.result);
+      }
+    });
   }
 }
