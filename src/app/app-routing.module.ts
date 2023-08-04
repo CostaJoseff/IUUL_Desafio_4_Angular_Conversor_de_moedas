c11b3c938c23513eae6ae6ea068c0b91eb42de39
Costa Joseff <rielisson.joseff.costa@ccc.ufcg.edu.br>
2023-08-04 18:09:25 -0300

    Lógica SPA adicionada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PaginaInicialComponent } from './components/pagina-inicial/pagina-inicial.component';
+import { AreaDeConversaoComponent } from './components/area-de-conversao/area-de-conversao.component';
+import { HistoricoComponent } from './components/historico/historico.component';
+
+const routes: Routes = [
+  {path: "", component: PaginaInicialComponent},
+  {path: "converter", component: AreaDeConversaoComponent},
+  {path: "historico", component: HistoricoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
