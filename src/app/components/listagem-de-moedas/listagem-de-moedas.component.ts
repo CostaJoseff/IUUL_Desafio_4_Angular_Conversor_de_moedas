e7ca88564d622e61480c2bc52783157268aeaebe
Costa Joseff <rielisson.joseff.costa@ccc.ufcg.edu.br>
2023-08-09 17:41:51 -0300

    lógica de filtragem adicionada

--- src/app/components/listagem-de-moedas/listagem-de-moedas.component.ts
+++ src/app/components/listagem-de-moedas/listagem-de-moedas.component.ts
@@ -9,15 +9,53 @@ import { APIConsumer } from '../../../assets/APIConsumer';
 export class ListagemDeMoedasComponent {
   private ApiConsumer: APIConsumer;
   public moedas: any[];
+  private moedasBCKP: any[];
+  public filtro: string;
+  private resultadosAnteriores: any[];
+  private indiceNovoResultado: number;
 
   constructor() {
     this.ApiConsumer = new APIConsumer();
     this.moedas = [];
+    this.moedasBCKP = [];
+    this.filtro = "";
+    this.resultadosAnteriores = [];
+    this.indiceNovoResultado = 0;
   }
 
   ngOnInit() {
     setTimeout(() => {
       this.moedas = this.ApiConsumer.getMoedas();
+      this.moedasBCKP = [...this.moedas];
     }, 1000);
+    
+  }
+
+  public filtrar(evento: KeyboardEvent) {
+    if (this.filtro.trim() === "") {
+      this.moedas = [...this.moedasBCKP];
+      this.resultadosAnteriores = [];
+      this.indiceNovoResultado = 0;
+
+    } else if ((evento.key == "Backspace" || evento.key == "Delete") && this.filtro.trim() != "") {
+      this.indiceNovoResultado--;
+      this.moedas = this.resultadosAnteriores[this.indiceNovoResultado];
+      this.resultadosAnteriores.splice(this.indiceNovoResultado, 1)
+    } else {
+      let cache: any = [];
+      this.moedas.forEach((moeda) => {
+        let temp = `${moeda.description} ${moeda.code}`;
+        if (temp.toUpperCase().includes(this.filtro.toUpperCase())) {
+          cache.push(moeda);
+        }
+      });
+      this.resultadosAnteriores.push(this.moedas);
+      this.indiceNovoResultado++;
+      this.moedas = cache;
+
+    }
+
+    console.log(this.resultadosAnteriores, [this.moedas]);
+    console.log(this.indiceNovoResultado);
   }
 }
